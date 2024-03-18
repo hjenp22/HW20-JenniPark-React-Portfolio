ca97a92b75a15efacf410f1fc0e14814c3696cd6
hjenp22 <hjipark22@gmail.com>
2024-03-18 10:15:30 -0700

    will redo with images

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,5 +1,5 @@
 import Project from "../components/Project";
-
+// import netFlexImage from "../assets/images/NetFlex.jpeg"; WILL FIGURE IMAGE OUT AND RESUBMIT
 
 export default function Portfolio() {
   // use an object for the project
@@ -9,31 +9,37 @@ export default function Portfolio() {
       name: "NETFLEX",
       githubLink: "https://github.com/Project2Team1GPR/NETFLEX",
       deployedLink: "https://project2-team1-netflex.onrender.com/login",
-      image: '../images/NetFlex.jpeg',
+    //   image: netFlexImage,
     },
     {
-        id: 2,
-        name: "ORM E-Commerce Back End",
-        githubLink: "https://github.com/hjenp22/ORM-E-Commerce-Back-End-HW13",
-        deployedLink: "https://project2-team1-netflex.onrender.com/login", //need to render
-        image: '../images/NetFlex.jpeg',
+      id: 2,
+      name: "ORM E-Commerce Back End",
+      githubLink: "https://github.com/hjenp22/ORM-E-Commerce-Back-End-HW13",
+      deployedLink: "https://github.com/hjenp22/ORM-E-Commerce-Back-End-HW13", 
+    //   image: "../images/NetFlex.jpeg",
+    },
+    {
+      id: 3,
+      name: "Server Side API Weather Dashboard",
+      githubLink: "https://github.com/hjenp22/Server-SideAPI-Weather-Dashboard",
+      deployedLink: "https://hjenp22.github.io/Server-SideAPI-Weather-Dashboard/", 
+    //   image: "../images/NetFlex.jpeg",
+    },
+    {
+        id: 4,
+        name: "NoSQL Social Network",
+        githubLink: "https://github.com/hjenp22/HW18NoSQL-SocialNetwork",
+        deployedLink: "https://hjenp22.github.io/HW18NoSQL-SocialNetwork/", 
+        // image: "../images/NetFlex.jpeg",
       },
-      {
-        id: 3,
-        name: "Server Side API Weather Dashboard",
-        githubLink: "https://github.com/hjenp22/Server-SideAPI-Weather-Dashboard",
-        deployedLink: "https://project2-team1-netflex.onrender.com/login", //need to render
-        image: '../images/NetFlex.jpeg',
-      },
-    //add 4 more
-    //https://github.com/hjenp22/ORM-E-Commerce-Back-End-HW13
+   
   ];
 
   return (
     <>
       <div>
         <div className="container mt-5 mb-5 p-3 py-md-5 px-md-3 portfolio-container">
-          <h1>Portfolio</h1>
+          <h5 className="portfolio-title">Portfolio</h5>
           <div className="row row-cols-1 row-cols-md-2 g-4">
             {projects.map((project) => (
               <div key={project.id} className="col">
