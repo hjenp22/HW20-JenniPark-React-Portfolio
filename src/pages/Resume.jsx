e30950511ec64de0c440a247f5d2ca0d86bff89f
hjenp22 <hjipark22@gmail.com>
2024-03-17 19:58:08 -0700

    resumeeeeee

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import "../pages/Resume.css";
 
 export default function Resume() {
   const resumeLink =
@@ -17,19 +18,87 @@ export default function Resume() {
           <Col>
             <div className="content">
               <h4>Work Experience</h4>
+
               <ul>
-                <li>
-                  <strong>JDM Consulting and Associates: Consultant</strong>{" "}
-                  <p>
-                    Led 6-month client collaboration project for Python-based solutions integrated with PostgreSQL databases. Managed 5-member cross-functional team, ensuring timely delivery and adherence to scope. Achieved 30% SQL query response time improvement and 25% database efficiency boost through performance optimization. Provided consultancy, achieving 80% adoption of best practices. Maintained 95% on-time delivery and 90% scope adherence. Collaborated on business requirements analysis, backend development, and project delivery with cross-functional teams.
-                  </p>
-                </li>
-                <li>
-                  <strong>Web Technologies:</strong> Django, Reach.js, HTML, CSS, JSON, Bootstrap, jQuery, AJAX
-                </li>
-                <li>
-                  <strong>Database Technologies:</strong> MongoDB, NoSQL, MySQL
-                </li>
+                <h5>JDM Consulting and Associates | Consultant</h5>{" "}
+                <ul>
+                  <li>
+                    Led 6-month client collaboration project for Python-based
+                    solutions integrated with PostgreSQL databases.
+                  </li>
+                  <li>
+                    Managed 5-member cross-functional team, ensuring timely
+                    delivery and adherence to scope.
+                  </li>
+                  <li>
+                    Achieved 30% SQL query response time improvement and 25%
+                    database efficiency boost through performance optimization.
+                  </li>
+                  <li>
+                    Provided consultancy, achieving 80% adoption of best
+                    practices.
+                  </li>
+                  <li>
+                    Maintained 95% on-time delivery and 90% scope adherence.
+                  </li>
+                  <li>
+                    Collaborated on business requirements analysis, backend
+                    development, and project delivery with cross-functional
+                    teams.
+                  </li>
+                </ul>
+              </ul>
+
+              <ul>
+                <h5>JPAIK | Database and Art Inventory Manager</h5>{" "}
+                <ul>
+                  <li>
+                    Spearheaded the establishment and meticulous maintenance of
+                    a comprehensive database encompassing 100+ clients,
+                    capturing intricate details, contact information, and
+                    personalized art preferences.
+                  </li>
+                  <li>
+                    Orchestrated the efficient management of an art inventory
+                    comprising 75+ pieces, collectively valued at over $1
+                    million.
+                  </li>
+                  <li>
+                    Significantly contributing to the expansion of the artist
+                    roster, I engaged in dedicated research, attended art fairs,
+                    and conducted studio visits across the Midwest to identify
+                    and onboard new talents.
+                  </li>
+                </ul>
+              </ul>
+
+              <ul>
+                <h5>JDM Consulting and Associates | Intern </h5>{" "}
+                <ul>
+                  <li>
+                    Conducted in-depth research, leading to an 80% improvement
+                    in project efficiency by implementing advanced data analysis
+                    techniques
+                  </li>
+                  <li>
+                    Collaborated with cross-functional teams to devise
+                    innovative client solutions, resulting in increased client
+                    satisfaction by 70%.
+                  </li>
+                  <li>
+                    Engaged directly with end users, addressing their needs and
+                    ensuring a seamless client experience.
+                  </li>
+                  <li>
+                    Developed and applied strong analytical and problem-solving
+                    skills, actively contributing to the creation of high-impact
+                    client deliverables.
+                  </li>
+                  <li>
+                    Participated in client meetings, enhancing communication
+                    skills and fostering client relationships.
+                  </li>
+                </ul>
               </ul>
             </div>
           </Col>
@@ -38,33 +107,96 @@ export default function Resume() {
           <Col>
             <div className="content">
               <h4>Technical Skills</h4>
+
               <ul>
+                <h7>
+                  <em>Programming Languages:</em>
+                </h7>
+                <ul>
+                  <li>Python, JavaScript, HTML/CSS, SQL, JSON</li>
+                </ul>
+
+                <h7>
+                  <em>Web Technologies:</em>
+                </h7>
+                <ul>
+                  <li>
+                    Django, React.js, HTML, CSS, JSON, Bootstrap, jQuery, AJAX
+                  </li>
+                </ul>
+
+                <h7>
+                  <em>Database Technologies:</em>
+                </h7>
+                <ul>
+                  <li>MongoDB, NoSQL, MySQL</li>
+                </ul>
+
+                <h7>
+                  <em>Development Tools & Platforms:</em>
+                </h7>
+                <ul>
+                  <li>
+                    Render, Heroku, Git, Bash, GitHub, Microsoft Office Suite
+                  </li>
+                </ul>
+
+                <h7>
+                  <em>Software Development:</em>
+                </h7>
+                <ul>
+                  <li>System analysis, Coding Standards</li>
+                </ul>
+
+                <h7>
+                  <em>Frontend Frameworks:</em>
+                </h7>
+                <ul>
+                  <li>Bootstrap, Handlebars.js, React</li>
+                </ul>
+
+                <h7>
+                  <em>Backend Frameworks:</em>
+                </h7>
+                <ul>
+                  <li>Django, Node.js</li>
+                </ul>
+
+                <h7>
+                  <em>APIs:</em>
+                </h7>
+                <ul>
+                  <li>Application Programming Interfaces</li>
+                </ul>
+
+                <h7>
+                  <em>Other:</em>
+                </h7>
+                <ul>
+                  <li>
+                    Tableau, Power BI, Data Structures, JavaScript Frameworks
+                  </li>
+                </ul>
+              </ul>
+            </div>
+
+            <div className="content">
+              <h4> Education</h4>
+              <h7>
+                <em>Northwestern University | Chicago IL</em>
+              </h7>
+              <ul>
+                <li>Full Stack Coding Certificate Candidate</li>
+              </ul>
+
+              <h7>
+                <em>Saint Xavier University | Chicago IL</em>
+              </h7>
+              <ul>
+                <li>Bachelor of Science in Biology Pre-Health Sciences</li>
+                <li>Graduate - Magna Cum Laude</li>
                 <li>
-                  <strong>Programming Languages:</strong> Python, JavaScript, HTML/CSS, SQL, JSON
-                </li>
-                <li>
-                  <strong>Web Technologies:</strong> Django, Reach.js, HTML, CSS, JSON, Bootstrap, jQuery, AJAX
-                </li>
-                <li>
-                  <strong>Database Technologies:</strong> MongoDB, NoSQL, MySQL
-                </li>
-                <li>
-                  <strong>Development Tools & Platforms:</strong> Render, Heroku, Git, Bash, GitHub, Microsoft Office Suite
-                </li>
-                <li>
-                  <strong>Software Development:</strong> System analysis, Coding Standards
-                </li>
-                <li>
-                  <strong>Frontend Frameworks:</strong> Bootstrap, Handlebars.js, React
-                </li>
-                <li>
-                  <strong>Backend Frameworks:</strong> Django, Node.js
-                </li>
-                <li>
-                  <strong>APIs:</strong> Application Programming Interfaces
-                </li>
-                <li>
-                  <strong>Other:</strong> Tableau, Power BI, Data Structures, JavaScript Frameworks
+                  Member of Beta Beta Beta- Biology National Honors Society
                 </li>
               </ul>
             </div>
